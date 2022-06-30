f25cd4a2fddb5147ea5dbb2f60fb100176af862d
fkymy <yuskefuku@gmail.com>
2022-06-30 18:52:23 +0900

    sample navbar

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import Link from 'next/link';
 import { useAccount } from 'wagmi';
 import styles from '../styles/Home.module.css';
 
@@ -14,24 +15,32 @@ const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>monoswap</title>
+        <title>Monoswap</title>
         <meta name='description' content='' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <main>
-        <div
-          style={{
-            position: 'fixed',
-            top: 0,
-            bottom: 0,
-            left: 0,
-            right: 0,
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        >
-          <ConnectButton />
+        <ConnectButton />
+
+        <div>
+          <h2 className='font-semibold text-2xl'>Order List</h2>
+          <ul>
+            <li>
+              <Link href='/orders/test'>
+                <a className='text-blue-700'>Order 1</a>
+              </Link>
+            </li>
+            <li>
+              <Link href='#'>
+                <a className='text-blue-700'>Order 2</a>
+              </Link>
+            </li>
+            <li>
+              <Link href='#'>
+                <a className='text-blue-700'>Order 2</a>
+              </Link>
+            </li>
+          </ul>
         </div>
       </main>
     </div>
