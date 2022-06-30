00aa521cae515ad03ca6e7e0f542be4d65b59523
fkymy <yuskefuku@gmail.com>
2022-06-30 12:48:09 +0900

    add connect wallet

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,7 +1,37 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import '@rainbow-me/rainbowkit/styles.css';
+
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { chain, configureChains, createClient, WagmiConfig } from 'wagmi';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
+// Configure Rainbowkit
+const { chains, provider } = configureChains(
+  [chain.polygonMumbai, chain.goerli, chain.mainnet, chain.polygon, chain.rinkeby],
+  [alchemyProvider({ alchemyId: process.env.ALCHEMY_ID }), publicProvider()],
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'Monoswap',
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
   return <Component {...pageProps} />;
 }
 
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,9 +1,16 @@
+import { ConnectButton } from '@rainbow-me/rainbowkit';
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
+import { useAccount } from 'wagmi';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
+  const { address, isConnected } = useAccount();
+
+  if (isConnected) {
+    console.log('Connected to: ', address);
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,7 +19,20 @@ const Home: NextPage = () => {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <main>
-        <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+        <div
+          style={{
+            position: 'fixed',
+            top: 0,
+            bottom: 0,
+            left: 0,
+            right: 0,
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <ConnectButton />
+        </div>
       </main>
     </div>
   );
