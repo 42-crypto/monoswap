d754aa21ca5f06fe10c1d324ce500b6303f9f2a5
fkymy <yuskefuku@gmail.com>
2022-07-03 07:36:00 +0900

    select nft

--- frontend/src/components/selectGive.tsx
+++ frontend/src/components/selectGive.tsx
@@ -123,84 +123,118 @@ const SelectGive = ({ showGive, setShowGive, addSelectedItem }) => {
   };
 
   return (
-    <div className='glass-modal rounded-tl-[20px] border-2 border-white/40 fixed top-0 right-0 h-screen w-1/2 mt-[87px]'>
-      {!nfts && (
-        <>
-          <div>Loading...</div>
-        </>
-      )}
-      <div className='flex flex-col items-start py-8 pl-8 pr-16'>
-        <div className='mb-6'>
-          <img className='' src='/double_arrow_right.png' alt='double_arrow_right' />
-        </div>
-        <div className='flex items-start space-x-8'>
-          {nfts && (
-            <div className='flex flex-col w-[356px] space-y-4'>
-              <form className='w-full'>
-                <label
-                  htmlFor='default-search'
-                  className='mb-2 text-sm font-medium text-[#8E849E] sr-only'
-                >
-                  Search
-                </label>
-                <div className='relative'>
-                  <div className='flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none'>
-                    <svg
-                      className='w-5 h-5 text-[#8E849E]'
-                      fill='none'
-                      stroke='currentColor'
-                      viewBox='0 0 24 24'
-                      xmlns='http://www.w3.org/2000/svg'
-                    >
-                      <path
-                        strokeLinecap='round'
-                        strokeLinejoin='round'
-                        strokeWidth='2'
-                        d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z'
-                      ></path>
-                    </svg>
-                  </div>
-                  <input
-                    type='search'
-                    id='default-search'
-                    className='block p-2 pl-10 w-full bg-transparent text-base text-[#8E849E] rounded-xl border-2 border-darkGray focus:ring-primary focus:border-primary '
-                    placeholder='Search'
-                    required
-                  />
-                </div>
-              </form>
-              <div className='overflow-y-auto no-scrollbar grid grid-cols-2 gap-4 h-[709px]'>
-                {nfts.map((nft: NFT, index: number) => (
-                  <div
-                    key={index}
-                    className='flex flex-col glass-inner-empty h-[222px] rounded-2xl border-2 border-white/50'
-                    onClick={() => selectNft(nft)}
-                  >
-                    <div className='glass-modal-inner rounded-[14px] overflow-hidden'>
-                      <img
-                        className='object-contain h-[164px] w-[164px] rounded-t-md p-2'
-                        src={nft.media[0].gateway}
-                      ></img>
-                    </div>
-                    <div className='flex flex-col item-start pt-[10px] pl-4'>
-                      <p className='text-white text-[12px] font-bold'>{nft.metadata.name}</p>
-                      <p className='text-white text-[12px]'>Axie Infinity</p>
-                    </div>
-                  </div>
-                ))}
-              </div>
-              <button
-                className='bg-blue-500 hover:bg-blue-700 text-white font-blod py-2 px-4 rounded'
-                onClick={addNft}
-              >
-                Add NFT
-              </button>
-            </div>
+    <>
+      {showGive && (
+        <div className='glass-modal rounded-tl-[20px] border-2 border-white/40 fixed top-0 right-0 h-screen w-1/2 mt-[87px]'>
+          {!nfts && (
+            <>
+              <div>Loading...</div>
+            </>
           )}
+          <div className='flex flex-col items-start py-8 pl-8 pr-16'>
+            <div
+              className='mb-6'
+              onClick={() => (showGive ? setShowGive(false) : setShowGive(true))}
+            >
+              <img className='' src='/double_arrow_right.png' alt='double_arrow_right' />
+            </div>
+            <div className='flex items-start space-x-8'>
+              {nfts && (
+                <div className='flex flex-col w-[356px] space-y-4'>
+                  <form className='w-full'>
+                    <label
+                      htmlFor='default-search'
+                      className='mb-2 text-sm font-medium text-[#8E849E] sr-only'
+                    >
+                      Search
+                    </label>
+                    <div className='relative'>
+                      <div className='flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none'>
+                        <svg
+                          className='w-5 h-5 text-[#8E849E]'
+                          fill='none'
+                          stroke='currentColor'
+                          viewBox='0 0 24 24'
+                          xmlns='http://www.w3.org/2000/svg'
+                        >
+                          <path
+                            strokeLinecap='round'
+                            strokeLinejoin='round'
+                            strokeWidth='2'
+                            d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z'
+                          ></path>
+                        </svg>
+                      </div>
+                      <input
+                        type='search'
+                        id='default-search'
+                        className='block p-2 pl-10 w-full bg-transparent text-base text-[#8E849E] rounded-xl border-2 border-darkGray focus:ring-primary focus:border-primary '
+                        placeholder='Search'
+                        required
+                      />
+                    </div>
+                  </form>
+                  <div className='overflow-y-auto no-scrollbar grid grid-cols-2 gap-4 h-[709px]'>
+                    {nfts.map((nft: NFT, index: number) => (
+                      <div
+                        key={index}
+                        className='flex flex-col glass-inner-empty h-[222px] rounded-2xl border-2 border-white/50'
+                        onClick={() => selectNft(nft)}
+                      >
+                        <div className='glass-modal-inner rounded-[14px] overflow-hidden flex items-center justify-center'>
+                          <img
+                            className='object-contain h-[164px] w-[164px] rounded-t-md p-2'
+                            src={nft.media[0].gateway}
+                          ></img>
+                        </div>
+                        <div className='flex flex-col item-start pt-[10px] pl-4'>
+                          <p className='text-white text-[12px] font-bold'>{nft.metadata.name}</p>
+                          <p className='text-white text-[12px]'>Axie Infinity</p>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                  <button
+                    className='bg-blue-500 hover:bg-blue-700 text-white font-blod py-2 px-4 rounded'
+                    onClick={addNft}
+                  >
+                    Add NFT
+                  </button>
+                </div>
+              )}
+              {Object.keys(selected).length !== 0 && (
+                <div className='flex flex-col items-center w-[290px]'>
+                  <div className='flex flex-col items-center space-y-3'>
+                    <p className='text-xl text-white font-bold '>{selected.metadata.name}</p>
+                    <div className='glass-modal-inner rounded-2xl overflow-hidden h-[178px] w-[198px] border border-white/60 flex items-center justify-center'>
+                      <img
+                        src={selected.media[0].gateway}
+                        alt={selected.metadata.name}
+                        className='object-contain h-[160px] w-[160px] p-2'
+                      />
+                    </div>
+
+                    <div className='flex justify-center space-x-1'>
+                      <div>
+                        <p className=' text-[#CBC6D2] text-base'>Owned by </p>
+                      </div>
+                      <div>
+                        <a suppressHydrationWarning={true} className='text-[#24D6DD] text-base'>
+                          {address ? address.substring(2, 8) : '89678'}
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                  <div></div>
+                </div>
+              )}
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
+
   return (
     <AnimatePresence exitBeforeEnter>
       {showGive && (
