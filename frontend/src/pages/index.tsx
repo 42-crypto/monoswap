aeca7445419e877563fd86a88d760dc7e3a3941b
yshkd <yushi.public@gmail.com>
2022-07-03 07:19:42 +0900

    Update index ui

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -6,6 +6,7 @@ import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import useSWR from 'swr';
 import { useCollection } from 'swr-firestore-v9';
+import React from 'react';
 
 import Layout from '@/components/layout';
 import OrderCard from '@/components/ordercard';
@@ -25,30 +26,20 @@ const IndexPage: NextPage = () => {
 
   const orderExeptFullfilled = orderData ? orderData.filter(order => order.fulfilled == false) : [];
 
+
   return (
     <Layout>
-      <div className='bg-background flex flex-direction: row'>
+      <div className='bg-background flex flex-direction: row max-w-6xl' style={{ margin: '5vh auto 0' }}>
         { gameData && (
             <aside className="w-4/12" aria-label="Sidebar">
-              <h5 className='font-semibold text-2xl text-white'>Game List</h5>
-              <div className="overflow-y-auto py-4 px-3 bg-background rounded ">
-                  <ul className="space-y-2">
-                    {gameData.map((game) => (
-                      <li key={game.name} className=''>
-                        <a href="#" className="flex items-center p-2 text-base font-semibold rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">                            <img className = 'w-10 h-w height-auto object-fit-cover' src= {game.imageUrl} />
-                            <span className="ml-3">{game.name}</span>
-                          </a>
-                      </li>
-                      ))}
-                   </ul>
-              </div>
+              <img src={"/filter.png"} alt="filter" />;
             </aside>
           )
         }
 
         {orderData && (
           <div className="w-full">
-            <ul className= 'items'>
+            <ul className= 'items justify-center justify-around'>
               {orderData.map(order => (
                 <>
                   <li key={order.id} className='item glass-outer border-2 border-white/40 p-5 rounded-2xl'>
