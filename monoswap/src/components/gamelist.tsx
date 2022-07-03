a4693f668372287cc0a115c20bf068f9481a789d
fkymy <yuskefuku@gmail.com>
2022-07-03 12:07:23 +0900

    deploy

--- monoswap/src/components/gamelist.tsx
+++ monoswap/src/components/gamelist.tsx
@@ -1,12 +1,9 @@
 import { Game } from '@/types';
-import filter from './filter.png';
 
-const GameList = (
-  games: Game[]
-) => { 
-  return(
-    <aside className="w-64" aria-label="Sidebar">
-      <img src={filter} alt="filter" />;
+const GameList = (games: Game[]) => {
+  return (
+    <aside className='w-64' aria-label='Sidebar'>
+      <img src='/filter.png' alt='filter' />;
     </aside>
-  )
-}
+  );
+};
